bd13d47acb2e84e4b3707f189f1c0daa82266610
zihad868 <zihadh868@gmail.com>
2024-07-16 16:52:16 +0600

    Connect MongoDB & Vercel Setup

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ require('dotenv').config()
 
 const port = process.env.Port || 5000;
 
-// CashMate
-// 3NN4RTy6RwNLJYVo
-
+//https://cash-mate-server.vercel.app/
 
 var uri = `mongodb://${process.env.DB_User}:${process.env.DB_Password}@ac-62j8ihz-shard-00-00.7lbrva6.mongodb.net:27017,ac-62j8ihz-shard-00-01.7lbrva6.mongodb.net:27017,ac-62j8ihz-shard-00-02.7lbrva6.mongodb.net:27017/?ssl=true&replicaSet=atlas-g1t94d-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,18 +20,20 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
 
-    
+    app.get('/product', (req, res) => {
+      res.send("Production Success")
+    })
 
 
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
