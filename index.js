4f5435799aa446687ce1d6b8b9de62678fcd3dcb
zihad868 <zihadh868@gmail.com>
2024-07-16 15:44:20 +0600

    Setup MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
 const port = process.env.Port || 5000;
+
+// CashMate
+// 3NN4RTy6RwNLJYVo
+
+
+var uri = `mongodb://${process.env.DB_User}:${process.env.DB_Password}@ac-62j8ihz-shard-00-00.7lbrva6.mongodb.net:27017,ac-62j8ihz-shard-00-01.7lbrva6.mongodb.net:27017,ac-62j8ihz-shard-00-02.7lbrva6.mongodb.net:27017/?ssl=true&replicaSet=atlas-g1t94d-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+    
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send("Cash Mate server is running")
