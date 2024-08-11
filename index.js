41bdbebbc633c98c8295c4ff434e4481f455cefb
zihad868 <zihadh868@gmail.com>
2024-08-11 12:50:48 +0600

    Connect MongoDB

--- index.js
+++ index.js
@@ -1,48 +1,19 @@
-const express = require('express');
-const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const app = express()
 require('dotenv').config()
 
-const port = process.env.Port || 5000;
+// Import MongoDB
+const connectMongoDB = require('./Models/db')
 
-//https://cash-mate-server.vercel.app/
+port = process.env.PORT || 5000;
 
-var uri = `mongodb://${process.env.DB_User}:${process.env.DB_Password}@ac-62j8ihz-shard-00-00.7lbrva6.mongodb.net:27017,ac-62j8ihz-shard-00-01.7lbrva6.mongodb.net:27017,ac-62j8ihz-shard-00-02.7lbrva6.mongodb.net:27017/?ssl=true&replicaSet=atlas-g1t94d-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// Connect MongoDB
+connectMongoDB()
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-
-    app.get('/product', (req, res) => {
-      res.send("Production Success")
-    })
-
-
-
-    // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-run().catch(console.dir);
-
-
-app.get('/', (req, res) => {
-    res.send("Cash Mate server is running")
+app.get('', (req, res) => {
+   res.send("Cash mate server is running ....")
 })
 
 app.listen(port, () => {
-    console.log("Server Listen on Port", port)
+   console.log(`Server is running on port ${port}`)
 })
